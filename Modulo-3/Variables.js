637ef5a40a2919a37d34363f0951d34b25f7722b
GastyBenitez <gastonbenitez0207@gmail.com>
2023-11-14 16:42:38 -0300

    variables y operadores

---
+++ Modulo-3/Variables.js
@@ -0,0 +1,29 @@
+var cajon;
+
+//String palabras, dentro de comillas dobles o simples
+
+var bootcamp = 'Henry';
+
+//Numeros pueden ser positivos, negativos, enteros o decimales
+
+var numeroEntero = 17;
+
+var numeroNgegativo = -4;
+
+var numeroDecimal = 11.75;
+
+//Booleanos, verdadero o falso, palabras reservadas que no van entre comillas
+
+var estoyEntendiendo = true;
+
+var estoyAburrido = false;
+
+//Undefined indefinido, aparece cuando no le asignamos valor a la variable
+
+var cajon1;
+
+console.log(cajon1);
+
+//Null representa la ausencia intencional de cualquier valor
+
+var sinNada = null;
---
+++ Modulo-3/operadores.js
@@ -0,0 +1,15 @@
+//console.log(3 + 5);
+
+//console.log(14-7);
+
+//console.log(4*6);
+
+//console.log(30/9);
+
+//console.log(95 % 4);
+
+
+//console.log(3 + 5 * 2 - 8); RESPETA LAS REGLAS DE PRECEDENCIA 
+
+//console.log((3 + 5) * 2 - 8);
+
